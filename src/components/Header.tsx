a83de68f90e95e0f21164a66aec328ab3f69f04a
hktari <bkamnik1995@gmail.com>
2022-09-07 11:21:47 +0200

    mobile side nav header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,34 @@
 import React from 'react'
 import '../css/components/Header.css'
 import logo from '../assets/images/logo.png'
+import avatarPlaceholder from '../assets/images/avatar-placeholder.png'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <header className='header'>
-      <img className='logo-top-left' src={logo} alt="geotagger logo" />
-      <button className='menu-btn'>
-        <span className="material-icons">
-          menu
-        </span>
-      </button>
-    </header>
+    <>
+      <header className='header w3-hide-medium w3-hide-large'>
+        <img className='logo-top-left' src={logo} alt="geotagger logo" />
+        <button className='menu-btn'>
+          <span className="material-icons">
+            menu
+          </span>
+        </button>
+      </header>
+      <nav className='side-nav'>
+        <div className="nav-header">
+          <button className="menu-btn">
+            <div className="material-icons">close</div>
+          </button>
+        </div>
+        <div className="nav-profile">
+          <img className='profile-img' src={avatarPlaceholder} alt="user profile"  />
+          <span className='body user-name'>Jacob Jones</span>
+        </div>
+      </nav>
+      <header className='header'></header>
+    </>
   )
 }
 
