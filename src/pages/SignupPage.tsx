9493795191c66e5fb205f4068a8a5aca593e109e
hktari <bkamnik1995@gmail.com>
2022-09-10 10:55:33 +0200

    add image picking and routing to signup page

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useRef, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from '../components/context/AuthProvider'
 import AsideSectionBrandDesktop from '../components/Signup/AsideSectionBrandDesktop'
 import '../css/pages/SignupPage.css'
+import authApi from '../services/authApi'
+import { fileToBase64 } from '../util/fileUtil'
 
 type Props = {}
 
@@ -13,42 +16,76 @@ const SignupPage = (props: Props) => {
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
     const [selectedImage, setSelectedImage] = useState('')
+    const [selectedImageBase64, setSelectedImageBase64] = useState('')
 
-    function onSubmit(event: React.FormEvent) {
+    const selectedImageRef = useRef<HTMLInputElement | null>(null);
+    const navigate = useNavigate()
+    const { login } = useAuth()
+
+    async function onSubmit(event: React.FormEvent) {
         event.preventDefault()
-        console.log('sign up')
+        try {
+            await authApi.signup(email, firstName, lastName, password, selectedImageBase64)
+            await login(email, password)
+            navigate('/dashboard')
+        } catch (error) {
+            console.error(error)
+            window.alert('Failed to sign up. Please try again.')
+        }
     }
+
+    async function onImagePicked(event: any) {
+        if (event.target.files.length > 0) {
+            setSelectedImage(event.target.files[0]);
+
+            const selectedImgEl: HTMLImageElement = document.getElementById('selectedImage') as HTMLImageElement
+            selectedImgEl.onload = () => {
+                URL.revokeObjectURL(selectedImgEl.src);
+            }
+
+            setSelectedImage(URL.createObjectURL(event.target.files[0]))
+
+            const imgBase64 = await fileToBase64(event.target.files[0])
+            setSelectedImageBase64(imgBase64)
+        }
+    }
+
 
     return (
         <div className="w3-row">
             <div className="w3-half w3-container w3-padding-large w3-padding-64">
                 <h1 className='header3 text-center'>Sign up</h1>
                 <p className="body text-center">Your name will appear on posts and your public profile</p>
-                <button className='select-profile-img btn btn-circle'>
+                <button className='select-profile-img btn btn-circle'
+                    onClick={() => selectedImageRef.current?.click()}>
                     <span className="material-icons">
                         person
                     </span>
-                    <img src={selectedImage} alt="" hidden={!selectedImage} />
+                    <img id='selectedImage' src={selectedImage} alt="" hidden={!selectedImage} />
+                    <input accept="image/png, image/jpeg" type='file' id='image'
+                        ref={selectedImageRef} style={{ display: 'none' }}
+                        onChange={onImagePicked} />
                 </button>
                 <form className='form' onSubmit={onSubmit}>
                     <label htmlFor="email">Email</label>
-                    <input value={email} onChange={e => setEmail(e.currentTarget.value)} type="text" id="email" />
+                    <input value={email} onChange={e => setEmail(e.currentTarget.value)} 
+                    type="email" id="email" required />
 
                     <label htmlFor="firstName">First Name</label>
                     <input value={firstName} onChange={e => setFirstName(e.currentTarget.value)}
-                        type="text" id="firstName" />
+                        type="text" id="firstName" required/>
 
                     <label htmlFor="lastName">last Name</label>
                     <input value={lastName} onChange={e => setLastName(e.currentTarget.value)}
-                        type="text" id="lastName" />
+                        type="text" id="lastName" required/>
 
                     <label htmlFor="password">Password</label>
                     <input value={password} onChange={e => setPassword(e.currentTarget.value)}
-                        type="password" id="password" />
+                        type="password" id="password" required/>
 
                     <label htmlFor="passwordConfirm">Confirm Password</label>
                     <input value={confirmPassword} onChange={e => setConfirmPassword(e.currentTarget.value)}
-                        type="password" id="passwordConfirm" />
+                        type="password" id="passwordConfirm" required/>
 
                     <input type="submit" className='btn btn-positive btn-block' value='SIGN UP' />
                 </form>
--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -10,11 +10,22 @@ async function login(email: string, password: string): Promise<User> {
     })
 }
 
+async function signup(email: string, firstName: string, lastName: string, password: string, imageBase64: string) {
+    return Promise.resolve({
+        id: 1,
+        firstName,
+        lastName,
+        email,
+        image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+    })
+}
+
+
 function logout() {
 
 }
 
-async function updateProfile(email: string, firstName: string, lastName: string, imageBase64: string) : Promise<User> {
+async function updateProfile(email: string, firstName: string, lastName: string, imageBase64: string): Promise<User> {
     return Promise.resolve({
         id: 0,
         email,
@@ -27,7 +38,8 @@ async function updateProfile(email: string, firstName: string, lastName: string,
 const authApi = {
     updateProfile,
     logout,
-    login
+    login,
+    signup
 }
 
 export default authApi;
