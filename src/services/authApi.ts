d5f1141cf548e943adf2bec747fb9d87480784e5
hktari <bkamnik1995@gmail.com>
2022-09-26 13:37:44 +0200

    add calls for profile update

--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -10,12 +10,12 @@ async function login(email: string, password: string): Promise<JWT> {
 }
 
 async function signup(email: string, firstName: string, lastName: string, password: string, imageBase64: string) {
-    return Promise.resolve({
-        id: 1,
+    return await restClient.post('/auth/signup', {
+        email,
         firstName,
         lastName,
-        email,
-        image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+        password,
+        imageBase64
     })
 }
 
@@ -25,26 +25,19 @@ function logout() {
 }
 
 async function updateProfile(email: string, firstName: string, lastName: string): Promise<User> {
-    return Promise.resolve({
-        id: 0,
-        email,
+    return restClient.put('/user/my-profile', {
         firstName,
-        lastName,
-        image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+        lastName
     })
 }
 
 async function updateProfileImage(email: string, imageBase64: string): Promise<User> {
-    return Promise.resolve({
-        id: 0,
-        firstName: 'TODO',
-        lastName: 'TODO',
-        email,
-        image: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+    return restClient.put('/user/my-profile/image', {
+        imageBase64
     })
 }
 
-async function changePassword(email: string, newPassword: string) : Promise<void> {
+async function changePassword(email: string, newPassword: string): Promise<void> {
     return Promise.resolve()
 }
 
