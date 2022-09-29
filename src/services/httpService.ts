fb0a2dc95cd515184e6d564c49a54740669f3485
hktari <bkamnik1995@gmail.com>
2022-09-29 14:06:48 +0200

    display info message on login

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -11,19 +11,25 @@ axios.defaults.headers.post['Content-Type'] = 'application/json';
 // }],
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response: any) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return { ...response.data };
-}, function (error: any) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject({ message: error.response.data?.message })
-});
+// axios.interceptors.response.use(
+//     function (response: any) {
+//         // Any status code that lie within the range of 2xx cause this function to trigger
+//         // Do something with response data
+//         return { ...response };
+//     }, function (error: any) {
+//         // Any status codes that falls outside the range of 2xx cause this function to trigger
+//         // Do something with response error
+//         return Promise.reject({ message: error.response?.data?.message, status: error.response?.status } as ApiError)
+//     });
 
 // restClient.defaults.validateStatus = function (status: number) {
 //     return status < 500; // Resolve only if the status code is less than 500
 // }
+
+
+export interface ApiError {
+    message: string
+}
 
 export function setAuthBearer(token: string) {
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
