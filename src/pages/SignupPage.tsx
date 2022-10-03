8c8035cef43d0b197cf6c3ab4b1182903b10151e
hktari <bkamnik1995@gmail.com>
2022-10-03 10:54:16 +0200

    update signup page flow

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import PickImageComponent from '../components/Common/PickImageComponent'
 import { useAuth } from '../components/context/AuthProvider'
+import InfoModal from '../components/modals/InfoModal'
 import AsideSectionBrandDesktop from '../components/Signup/AsideSectionBrandDesktop'
 import LayoutWithBrand from '../css/pages/LayoutWithBrand'
 import '../css/pages/SignupPage.css'
@@ -18,56 +19,85 @@ const SignupPage = (props: Props) => {
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
 
+    const [displayInfo, setDisplayInfo] = useState(false)
+    const [infoMessage, setInfoMessage] = useState('')
+
     const selectedImageBase64 = useRef<string>('')
 
     const navigate = useNavigate()
     const { login } = useAuth()
 
+    function showInfo(message: string) {
+        setInfoMessage(message)
+        setDisplayInfo(true)
+    }
+
     async function onSubmit(event: React.FormEvent) {
         event.preventDefault()
+        let signupSuccess = false
+
         try {
             await authApi.signup(email, firstName, lastName, password, selectedImageBase64.current)
+            signupSuccess = true
+            showInfo('Successfully created profile')
+        } catch (error: any) {
+            console.error(error)
+            showInfo('Failed to sign up: ' + error?.message)
+        }
+
+        let autologinSuccess = false
+        try {
             await login(email, password)
+            autologinSuccess = true
             navigate('/dashboard')
         } catch (error) {
+            console.error('failed to login automatically')
             console.error(error)
-            window.alert('Failed to sign up. Please try again.')
+        }
+
+        if (signupSuccess && !autologinSuccess) {
+            navigate('/login')
         }
     }
 
     return (
-        <LayoutWithBrand>
-            <h1 className='header3 text-center'>Sign up</h1>
-            <p className="body text-center">Your name will appear on posts and your public profile</p>
-            <PickImageComponent onImagePicked={(img) => selectedImageBase64.current = img} />
-            <form className='form' onSubmit={onSubmit}>
-                <label htmlFor="email">Email</label>
-                <input value={email} onChange={e => setEmail(e.currentTarget.value)}
-                    type="email" id="email" required />
+        <>
+            <LayoutWithBrand>
+                <h1 className='header3 text-center'>Sign up</h1>
+                <p className="body text-center">Your name will appear on posts and your public profile</p>
+                <PickImageComponent onImagePicked={(img) => selectedImageBase64.current = img} />
+                <form className='form' onSubmit={onSubmit}>
+                    <label htmlFor="email">Email</label>
+                    <input value={email} onChange={e => setEmail(e.currentTarget.value)}
+                        type="email" id="email" required />
 
-                <label htmlFor="firstName">First Name</label>
-                <input value={firstName} onChange={e => setFirstName(e.currentTarget.value)}
-                    type="text" id="firstName" required />
+                    <label htmlFor="firstName">First Name</label>
+                    <input value={firstName} onChange={e => setFirstName(e.currentTarget.value)}
+                        type="text" id="firstName" required />
 
-                <label htmlFor="lastName">last Name</label>
-                <input value={lastName} onChange={e => setLastName(e.currentTarget.value)}
-                    type="text" id="lastName" required />
+                    <label htmlFor="lastName">last Name</label>
+                    <input value={lastName} onChange={e => setLastName(e.currentTarget.value)}
+                        type="text" id="lastName" required />
 
-                <label htmlFor="password">Password</label>
-                <input value={password} onChange={e => setPassword(e.currentTarget.value)}
-                    type="password" id="password" required />
+                    <label htmlFor="password">Password</label>
+                    <input value={password} onChange={e => setPassword(e.currentTarget.value)}
+                        type="password" id="password" required />
 
-                <label htmlFor="passwordConfirm">Confirm Password</label>
-                <input value={confirmPassword} onChange={e => setConfirmPassword(e.currentTarget.value)}
-                    type="password" id="passwordConfirm" required />
+                    <label htmlFor="passwordConfirm">Confirm Password</label>
+                    <input value={confirmPassword} onChange={e => setConfirmPassword(e.currentTarget.value)}
+                        type="password" id="passwordConfirm" required />
 
-                <input type="submit" className='btn btn-positive btn-block' value='SIGN UP' />
-            </form>
-            <div className="form-footer">
-                Already have an account ? <Link to="/login" className='link'>Login</Link>
-            </div>
+                    <input type="submit" className='btn btn-positive btn-block' value='SIGN UP' />
+                </form>
+                <div className="form-footer">
+                    Already have an account ? <Link to="/login" className='link'>Login</Link>
+                </div>
 
-        </LayoutWithBrand>
+            </LayoutWithBrand>
+
+            <InfoModal isOpen={displayInfo} handleClose={() => setDisplayInfo(false)} title='Login Failed'
+                message={infoMessage} onFinished={result => setDisplayInfo(false)} />
+        </>
     )
 }
 
