e20825867389bcb14855ddcf408b90b3070eeed3
hktari <bkamnik1995@gmail.com>
2024-08-30 15:09:16 +0200

    style form input

--- src/components/Common/FormInput.tsx
+++ src/components/Common/FormInput.tsx
@@ -7,11 +7,15 @@ const FormInput = ({ title, name, type, required }: Props) => {
   const { register } = useFormContext()
 
   return (
-    <div>
-      <label className="" htmlFor={name}>
+    <div className="flex flex-col gap-2">
+      <label className="font-medium" htmlFor={name}>
         {title}
       </label>
-      <input {...register(name, { required })} type={type} />
+      <input
+        className="box-border rounded-sm border-b-[0.5px] border-black focus:px-3 py-3 transition-all focus:bg-patina-100 outline-none focus-visible:border-patina-500"
+        {...register(name, { required })}
+        type={type}
+      />
     </div>
   )
 }
