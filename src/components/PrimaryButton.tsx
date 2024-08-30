990d5aa83b5bbcf0744c9ac4070c64f366c8ba19
hktari <bkamnik1995@gmail.com>
2024-08-30 15:18:29 +0200

    add 'block' prop to PrimaryButton

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const PrimaryButton = ({
-  onClick,
-  type,
-  children,
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+type Props = {
+  block?: boolean
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
+const PrimaryButton = (props: Props) => {
+  const buttonProps = { ...props, block: undefined }
+  const block = props.block ? 'w-full' : ''
   return (
     <button
-      type={type}
-      className="h-12 w-32 rounded bg-patina-400 text-white hover:bg-patina-200 hover:text-slate-800 hover:ring-2 hover:ring-patina-400"
-      onClick={onClick}
+      {...props}
+      className={`h-12 w-32 rounded bg-patina-400 text-white hover:bg-patina-200 hover:text-slate-800 hover:ring-2 hover:ring-patina-400 ${block}`}
     >
-      {children}
+      {props?.children}
     </button>
   )
 }
