7168f71c719a66e57461159b63e94cebdd53be54
hktari <bkamnik1995@gmail.com>
2024-08-30 15:24:59 +0200

    add styling for error state

--- src/components/Common/FormInput.tsx
+++ src/components/Common/FormInput.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import { useFormContext } from 'react-hook-form'
+import { useFormContext, useFormState } from 'react-hook-form'
 
 type Props = { name: string } & React.InputHTMLAttributes<HTMLInputElement>
 
 const FormInput = ({ title, name, type, required }: Props) => {
   const { register } = useFormContext()
-
+  const { errors } = useFormState()
+  const hasError = errors[name]
   return (
     <div className="flex flex-col gap-2">
       <label className="font-medium" htmlFor={name}>
         {title}
       </label>
       <input
-        className="box-border rounded-sm border-b-[0.5px] border-black focus:px-3 py-3 transition-all focus:bg-patina-100 outline-none focus-visible:border-patina-500"
+        className={`box-border rounded-sm border-b-[0.5px] border-black py-3 focus:border-2 focus:bg-patina-100 focus:px-3 focus:outline-none focus:ring-patina-800 focus-visible:border-patina-500 ${hasError ? 'border-red-500' : 'border-patina-200'}`}
         {...register(name, { required })}
         type={type}
       />
