ebfed2297aba65faef81e121a1b47fce31e28fb1
hktari <bkamnik1995@gmail.com>
2024-09-01 16:04:04 +0200

    use LocationImageCard component

--- src/components/Dashboard/LocationImageCardGuess.tsx
+++ src/components/Dashboard/LocationImageCardGuess.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { LocationImage } from '../../services/interface'
 import SampleLocationImage from '../../assets/images/location-img-sample01.png'
+import LocationImageCard from '../LocationImageCard'
 
 type LocationImageGuessCardProps = {
   locationImage: LocationImage
@@ -14,15 +15,9 @@ const LocationImageGuessCard = ({
     <Link
       state={locationImage}
       to={'/location/guess'}
-      className="group relative block overflow-hidden rounded-lg"
+      className="group relative inline-block"
     >
-      {/* TODO: remove hardcoded image after cloudinary implementation */}
-      <img
-        className="h-48 w-72 max-w-md"
-        src={SampleLocationImage}
-        alt={locationImage.address}
-      />
-      <div className="absolute inset-0 z-10 bg-patina-400 opacity-75 group-hover:opacity-90"></div>
+      <LocationImageCard {...locationImage} />
       <div className="absolute inset-0 z-20 flex items-center justify-center text-3xl font-medium text-white transition-transform group-hover:scale-105">
         {locationImage.guessResult?.errorInMeters} m
       </div>
