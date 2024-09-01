90c04a67988bfd4f2e3afadac6c97f98fe12ba48
hktari <bkamnik1995@gmail.com>
2024-09-01 04:00:28 +0200

    style the SearchStreet component

--- src/components/Common/SearchStreetComponent.tsx
+++ src/components/Common/SearchStreetComponent.tsx
@@ -1,36 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import osmApi from '../../services/osmApi'
-import { GeocoderAutocomplete } from '@geoapify/geocoder-autocomplete';
-import '../../css/components/common/SearchStreetComponent.css'
-import { OsmAddress } from '../../services/osm.interface';
+import { GeocoderAutocomplete } from '@geoapify/geocoder-autocomplete'
+import { OsmAddress } from '../../services/osm.interface'
 
 type SearchStreetComponentProps = {
-    onAddressPicked: (addressInfo: OsmAddress) => void
+  onAddressPicked: (addressInfo: OsmAddress) => void
 }
 
-const SearchStreetComponent = ({ onAddressPicked }: SearchStreetComponentProps) => {
-    useEffect(() => {
-        const autocomplete = new GeocoderAutocomplete(
-            document.getElementById("autocomplete")!,
-            process.env.REACT_APP_OSM_API_KEY!,
-            {
-                type: 'street'
-            });
-
-        autocomplete.on('select', (location) => {
-            onAddressPicked(location)
-        });
-
-        autocomplete.on('suggestions', (suggestions) => {
-            // process suggestions here
-        });
-    }, [])
-
-    return (
-        <div id="autocomplete" className="autocomplete-container location-address">
-            <label htmlFor='address' className="body">Location</label>
-        </div>
+const SearchStreetComponent = ({
+  onAddressPicked,
+}: SearchStreetComponentProps) => {
+  useEffect(() => {
+    const autocomplete = new GeocoderAutocomplete(
+      document.getElementById('autocomplete')!,
+      process.env.REACT_APP_OSM_API_KEY!,
+      {
+        type: 'street',
+      },
     )
+
+    autocomplete.on('select', (location) => {
+      onAddressPicked(location)
+    })
+
+    autocomplete.on('suggestions', (suggestions) => {
+      // process suggestions here
+    })
+  }, [])
+
+  return (
+    <div key={1} id="autocomplete" className="relative space-y-2">
+      <label className="text-2xl">Location</label>
+    </div>
+  )
 }
 
 export default SearchStreetComponent
