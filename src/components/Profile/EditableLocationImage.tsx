09f882e7b472b4da211e9bdb9818c31f01e8d91e
hktari <bkamnik1995@gmail.com>
2024-09-01 14:35:36 +0200

    run prettier

--- src/components/Profile/EditableLocationImage.tsx
+++ src/components/Profile/EditableLocationImage.tsx
@@ -6,74 +6,83 @@ import { useLocationsContext } from '../context/LocationProvider'
 import DeleteLocationModal from '../modals/DeleteLocationModal'
 
 type EditableLocationImageProps = {
-    locationImage: LocationImage
+  locationImage: LocationImage
 }
 
-const EditableLocationImage = ({ locationImage }: EditableLocationImageProps) => {
+const EditableLocationImage = ({
+  locationImage,
+}: EditableLocationImageProps) => {
+  const [deleteImageModalOpen, setDeleteImageModalOpen] =
+    useState<boolean>(false)
+  const { deleteLocation } = useLocationsContext()
+  const navigate = useNavigate()
 
-    const [deleteImageModalOpen, setDeleteImageModalOpen] = useState<boolean>(false)
-    const { deleteLocation } = useLocationsContext()
-    const navigate = useNavigate()
+  function onClick(ev: React.MouseEvent<HTMLDivElement>) {
+    ev.preventDefault()
+    navigate('/location/guess', {
+      state: locationImage,
+    })
+  }
 
-    function onClick(ev: React.MouseEvent<HTMLDivElement>) {
-        ev.preventDefault()
-        navigate('/location/guess', {
-            state: locationImage
-        })
+  function onDeleteLocation(event: React.MouseEvent<HTMLButtonElement>) {
+    event.stopPropagation()
+    setDeleteImageModalOpen(true)
+  }
+
+  function onEditLocation(event: React.MouseEvent<HTMLButtonElement>) {
+    event.stopPropagation()
+    navigate('/location/edit', {
+      state: locationImage,
+    })
+  }
+
+  function toggleHoverOnParent(
+    ev: React.MouseEvent<HTMLButtonElement>,
+    noHover: boolean,
+  ) {
+    ev.currentTarget.parentElement?.classList.toggle('no-hover', noHover)
+  }
+
+  async function deleteImage() {
+    try {
+      await deleteLocation(locationImage.id)
+    } catch (error) {
+      console.error(error)
+      window.alert('Failed to delete image')
     }
+  }
 
-    function onDeleteLocation(event: React.MouseEvent<HTMLButtonElement>) {
-        event.stopPropagation()
-        setDeleteImageModalOpen(true);
-    }
-
-    function onEditLocation(event: React.MouseEvent<HTMLButtonElement>) {
-        event.stopPropagation()
-        navigate('/location/edit', {
-            state: locationImage
-        })
-    }
-
-    function toggleHoverOnParent(ev: React.MouseEvent<HTMLButtonElement>, noHover: boolean) {
-        ev.currentTarget.parentElement?.classList.toggle('no-hover', noHover)
-    }
-
-    async function deleteImage() {
-        try {
-            await deleteLocation(locationImage.id)
-        } catch (error) {
-            console.error(error)
-            window.alert('Failed to delete image')
-        }
-
-    }
-
-    return (
-        <div onClick={onClick} className=''>
-            <img src={locationImage.imageUrl} alt={locationImage.address} />
-            <button
-                onMouseOver={ev => toggleHoverOnParent(ev, true)}
-                onMouseLeave={ev => toggleHoverOnParent(ev, false)}
-                onClick={onDeleteLocation} className="">
-                <span className="material-icons">close</span>
-            </button>
-            <button
-                onMouseOver={ev => toggleHoverOnParent(ev, true)}
-                onMouseLeave={ev => toggleHoverOnParent(ev, false)}
-                onClick={onEditLocation} className="">
-                <span className="material-icons">edit</span>
-            </button>
-            <DeleteLocationModal
-                isOpen={deleteImageModalOpen}
-                onChoicePicked={(areYouSure) => {
-                    if (areYouSure) {
-                        deleteImage()
-                    }
-                    setDeleteImageModalOpen(false)
-                }}
-                handleClose={() => setDeleteImageModalOpen(false)} />
-        </div>
-    )
+  return (
+    <div onClick={onClick} className="">
+      <img src={locationImage.imageUrl} alt={locationImage.address} />
+      <button
+        onMouseOver={(ev) => toggleHoverOnParent(ev, true)}
+        onMouseLeave={(ev) => toggleHoverOnParent(ev, false)}
+        onClick={onDeleteLocation}
+        className=""
+      >
+        <span className="material-icons">close</span>
+      </button>
+      <button
+        onMouseOver={(ev) => toggleHoverOnParent(ev, true)}
+        onMouseLeave={(ev) => toggleHoverOnParent(ev, false)}
+        onClick={onEditLocation}
+        className=""
+      >
+        <span className="material-icons">edit</span>
+      </button>
+      <DeleteLocationModal
+        isOpen={deleteImageModalOpen}
+        onChoicePicked={(areYouSure) => {
+          if (areYouSure) {
+            deleteImage()
+          }
+          setDeleteImageModalOpen(false)
+        }}
+        handleClose={() => setDeleteImageModalOpen(false)}
+      />
+    </div>
+  )
 }
 
 export default EditableLocationImage
