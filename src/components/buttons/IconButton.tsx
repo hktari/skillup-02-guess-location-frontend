58d0412214eb0c2c211f7af0dfe64c7dce93388b
hktari <bkamnik1995@gmail.com>
2024-09-03 16:43:58 +0200

    add a switch to set backgroundColor inside IconButton

--- src/components/buttons/IconButton.tsx
+++ src/components/buttons/IconButton.tsx
@@ -3,12 +3,19 @@ import React from 'react'
 interface FloatingIconButtonProps
   extends React.HtmlHTMLAttributes<HTMLButtonElement> {
   icon: string
-  backgroundColor: string
+  backgroundColor: 'red' | 'gray' | 'green'
   position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
 }
 
 const FloatingIconButton = (props: FloatingIconButtonProps) => {
   const { icon, position, backgroundColor = 'gray' } = props
+  const buttonProps = {
+    ...props,
+    icon: undefined,
+    position: undefined,
+    backgroundColor: undefined,
+  }
+
   let positionClass = ''
   switch (position) {
     case 'top-left':
@@ -25,11 +32,24 @@ const FloatingIconButton = (props: FloatingIconButtonProps) => {
       break
   }
 
-  let backgroundColorClass = `bg-${backgroundColor}-200 hover:ring-${backgroundColor}-700`
+  let backgroundColorClass = ''
+  switch (backgroundColor) {
+    case 'red':
+      backgroundColorClass = 'bg-red-200 hover:ring-red-700'
+      break
+    case 'gray':
+      backgroundColorClass = 'bg-gray-200 hover:ring-gray-700'
+      break
+    case 'green':
+      backgroundColorClass = 'bg-emerald-200 hover:ring-emerald-700'
+      break
+    default:
+      throw new Error('Invalid background color')
+  }
 
   return (
     <button
-      {...props}
+      {...buttonProps}
       className={`hover absolute m-2 flex items-center justify-center rounded-md border-none p-1 transition-transform hover:scale-105 hover:ring-2 ${backgroundColorClass} ${positionClass}`}
     >
       <span className="material-icons">{icon}</span>
